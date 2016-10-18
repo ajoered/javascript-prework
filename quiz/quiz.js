25364b9497b37cfda723d7ce3c3f33fdacc46dcd
Alvarojoered <arojaspen@gmail.com>
2016-10-18 14:29:39 +0200

    Complete quiz.js

--- quiz/quiz.js
+++ quiz/quiz.js
@@ -14,7 +14,7 @@ questionName();
 var responses = []
 
 function questionStupid() {
-	var answer = prompt("Do you want chocolate?");
+	var answer = prompt("Is JJF the best surfer in the world?");
 	if (answer.toLowerCase() === "yes") {
 		answer = true
 	} else if (answer.toLowerCase() === "no") {
@@ -30,23 +30,70 @@ questionStupid();
 
 //Iteration 3
 function questionMultiple() {
-	var food = prompt("What do you prefer?: Chocolate, Pizza or Sushi?");
-	food = food.toLowerCase();
-	switch (food) {
-		case "chocolate":
-        	food = "Chocolate"
+	var worldtitles = prompt("How many titles has Kelly Slater won?: three, six or twelve?");
+	switch (worldtitles) {
+		case "three":
+        	worldtitles = false
         break;
-    	case "pizza":
-        	food = "Pizza"
+    	case "six":
+        	worldtitles = false
         break;
-    	case "Sushi":
-        	food = "Sushi"
+    	case "twelve":
+        	worldtitles = true
         break;
         default:
-        	"Please stick to the answers provded!"
+        	"Please stick to the answers provided!"
         return questionMultiple();
 	}
 	responses.push(questionMultiple);
 	}
 
 questionMultiple();
+
+//Iteration 4
+function questionMath() {
+	var answer = prompt("What is the square root of 144?");
+	if (answer === 12) {
+		answer = true;
+	} else if (answer !== 12) {
+		answer = false;
+	} else if (isNaN(answer)){
+		alert("Please enter an integer");
+		return questionStupid();
+	}
+	responses.push(questionStupid);
+}
+
+questionMath();
+
+
+function evaluate (responsesArray) {
+	totalTrue = 0
+	totalFalse = 0
+
+	for (var i = 0; i < responsesArray.length; i++) {
+		if (responsesArray[i] = true){
+			totalTrue += 1;
+	} else {
+			totalFalse += 1;
+	}
+	}
+
+	user.correct = totalTrue;
+	user.failed = totalFalse;
+
+	showResults();
+}
+
+function showResults() {
+	alert('Hola ' + user.name + '!.. You have ' + user.correct + ' correct answers and ' + user.failed + ' incorrect answers.' );
+}
+
+evaluate(responses);
+
+
+
+
+
+
+
